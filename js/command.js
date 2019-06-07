5aec7a135a1184e099826d93d03557611c0a99e5
wboray <wboray@yandex.ru>
2019-06-07 10:03:35 +0300

    html5 video

--- js/command.js
+++ js/command.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     const sheetCommand = document.querySelector('#sheet-command');
+    const sheetCommandItems = document.querySelectorAll('.sheet-command__people');
+    
 
 
     sheetCommand.addEventListener('click', function(event){
@@ -9,6 +11,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         //console.log(target);
         if (target.parentNode.classList.contains('sheet-command__title')){
             let sheetCommandContent = target.parentNode;
+
+            sheetCommandItems.forEach(element => {
+                if (element != sheetCommandContent.parentNode.parentNode) element.classList.remove('active');    
+            });
             sheetCommandContent.parentNode.parentNode.classList.toggle('active');
         }
     })
